265575584136b56d8da8a22c70d0c1ce3dfd3fb7
NSkutt <nskutt@protonmail.com>
2022-04-08 13:43:57 -0500

    basic arithmetic functions added

---
+++ calc.js
@@ -0,0 +1,21 @@
+let sum;
+function add(x,y){
+	sum = 0;
+	sum = x + y;
+	return sum;
+};
+function subtract(x,y){
+	sum = 0;
+	sum = x - y;
+	return sum;
+};
+function multiply(x,y){
+	sum = 0;
+	sum = x*y;
+	return sum;
+};
+function divide(x,y){
+	sum = 0;
+	sum = x/y;
+	return sum;
+};
